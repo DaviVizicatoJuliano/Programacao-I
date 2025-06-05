e32fbb2b75cb3108d3bab41abea455f5fce4a790
Davi Vizicato Juliano <davi1234.juliano@gmail.com>
2025-06-05 13:45:20 -0300

    Exercicios de Vetor em C#

---
+++ Aula3_Serjao/Aula3_Serjao/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula3_Serjao
+{
+    internal class Program
+    {
+        public class Livro
+        {
+            public string titulo;
+            public string autor;
+
+            public void ExibirDados()
+            {
+                Console.WriteLine($"Titulo do Livro: {this.titulo} - Autor: {this.autor}");
+            }
+
+            public Livro()
+            {
+                Console.Write("Escreva o Título do Livro: ");
+                this.titulo = Console.ReadLine();
+                Console.Write("Escreva o Autor: ");
+                this.autor = Console.ReadLine();
+            }
+        }
+        static void Main(string[] args) 
+        {
+            Livro[] livro = new Livro[3];
+
+            livro[0] = new Livro();
+            livro[1] = new Livro();
+            livro[2] = new Livro();
+
+            foreach (var livroatual in livro)
+            {
+                livroatual.ExibirDados();
+            }
+        }
+    }
+}
